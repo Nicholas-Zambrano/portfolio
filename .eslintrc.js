68f00ebb16f7741f23c85130d776f7ea7a7e9e17
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-09-16 15:57:45 +0100

    removed file

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // ...other ESLint configurations...
-    rules: {
-      // ...other rules...
-      'jsx-a11y/img-redundant-alt': 'warn', // Change 'error' to 'warn'
-    },
-  };
-  
