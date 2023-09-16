df56922f71426f3ddc8c17668fa9880f1e307de7
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-09-16 15:47:10 +0100

    added an esling file for the warnings

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // ...other ESLint configurations...
+    rules: {
+      // ...other rules...
+      'jsx-a11y/img-redundant-alt': 'warn', // Change 'error' to 'warn'
+    },
+  };
+  
